6d40ffc5e4c51398dd97052089c98516714b1724
creepyguy256 <83246448+creepyguy256@users.noreply.github.com>
2021-05-21 22:24:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ async def on_ready():
     print("Bot is ready!")
 
 @client.command()
+async def rule(ctx):
+    await ctx.send("Rule lol")
+
 async def hello(ctx):
-    await ctx.send("Hi!")
+    await ctx.send("Hello!")
 
 client.run(token)
